d5db380a63386c530d6c66330134deda7361b8db
El Amine <noah.mahamoud49@icloud.com>
2025-08-27 17:44:03 +0200

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Path dossier = Paths.get("C:\\Users\\noahm\\Documents");
+    }
+}
