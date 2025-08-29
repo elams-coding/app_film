7d34a762fae4b745ccd66e80811878ebf8144e7a
El Amine <noah.mahamoud49@icloud.com>
2025-08-29 16:25:27 +0200

    Changement de nom de la classe.
    Lecture des fichiers grâce à la classe DossierRessource

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+final class Main {
+    public static void main(String[] args) throws URISyntaxException {
+        DossierRessource.lireContenu();
+    }
+}
